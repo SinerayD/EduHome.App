23cfe507e911d663beff9baeeaa61784518ad6b0
Sineray Dayday <sineraytd.code.edu@az>
2023-07-15 02:09:56 +0400

    Add project files.

---
+++ EduHome.App/Context/EduHomeDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using EduHomeCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHomeApp.Context
+{
+    public class EduHomeDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public EduHomeDbContext(DbContextOptions<EduHomeDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ EduHome.App/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello.App.areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
