e09b81e82233a9b8b7eb74cab121674db67ab9d3
Sineray Dayday <sineraytd.code.edu@az>
2023-07-16 14:49:28 +0400

    teacher and position table relations build

---
+++ EduHome.Core/Entities/Position.cs
@@ -0,0 +1,21 @@
+﻿using EduHomeCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EduHome.Core.Entities
+{
+    public class Position:BaseModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(30)]
+        [Display(Name = "Position Name")]
+        public string Name { get; set; }
+        [Key]
+        public List<Teacher>? Teachers { get; set; }
+    }
+}
---
+++ EduHome.Core/Entities/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.ComponentModel;
+using EduHomeCore.Entities;
+
+namespace EduHome.Core.Entities
+{
+    public class Teacher :BaseModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(30)]
+        [Display(Name = "Teacher FullName")]
+        public string FullName { get; set; }
+        public string? Image { get; set; }
+        public string Title { get; set; }
+        public string Links { get; set; }   
+
+        public int PositionId { get; set; }
+        public Position? Position { get; set; }
+        [NotMapped]
+        public IFormFile? FormFile { get; set; }
+    }
+}
